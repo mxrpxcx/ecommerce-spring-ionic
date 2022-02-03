f582aa117f7657fa770cf07fdd919ab327fef843
Godoy <renanfgfg@gmail.com>
2022-02-03 03:37:55 -0300

    instanciando objetos

--- src/main/java/com/gxdxy/curso/CursoJavaECommerceApplication.java
+++ src/main/java/com/gxdxy/curso/CursoJavaECommerceApplication.java
@@ -1,13 +1,32 @@
 package com.gxdxy.curso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoJavaECommerceApplication {
+import com.gxdxy.curso.domain.Categoria;
+import com.gxdxy.curso.repository.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoJavaECommerceApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoJavaECommerceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria c1 = new Categoria(null, "Informática");
+		Categoria c2 = new Categoria(null, "Escritório");
+		
+		categoriaRepo.saveAll(Arrays.asList(c1,c2));
+		
 	}
 
 	/* Implementacao do find by id java 8
