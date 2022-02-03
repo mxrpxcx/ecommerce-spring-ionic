005da6958b2e3c160104fc85e5576f5721888487
Godoy <renanfgfg@gmail.com>
2022-02-02 21:56:18 -0300

    Projeto iniciado

---
+++ src/main/java/com/gxdxy/curso/CursoJavaECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.gxdxy.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoJavaECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoJavaECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gxdxy/curso/CursoJavaECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gxdxy.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoJavaECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
