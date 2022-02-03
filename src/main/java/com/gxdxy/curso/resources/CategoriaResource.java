28c6bb260f5284bda1a3a533b9e9e2f3888410f7
Godoy <renanfgfg@gmail.com>
2022-02-02 22:26:14 -0300

    Primeira classe dominio, categoria

--- src/main/java/com/gxdxy/curso/resources/CategoriaResource.java
+++ src/main/java/com/gxdxy/curso/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.gxdxy.curso.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.gxdxy.curso.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST funcionando";
+	public List<Categoria> listar() {
+		Categoria c1 = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(c1);
+		lista.add(c2);
+		
+		return lista;
 	}
 	
 }
