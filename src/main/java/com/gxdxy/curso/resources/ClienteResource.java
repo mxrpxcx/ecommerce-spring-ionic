6c85fec71c94a6965d91b63c140ef51b8808a792
Godoy <renanfgfg@gmail.com>
2022-02-08 05:02:31 -0300

    tirando referencia ciclica, criando resource e service pra cliente, endpoint por id cliente

---
+++ src/main/java/com/gxdxy/curso/resources/ClienteResource.java
@@ -0,0 +1,31 @@
+package com.gxdxy.curso.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gxdxy.curso.domain.Cliente;
+import com.gxdxy.curso.service.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> listar(@PathVariable Integer id) {
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+}
+
+
