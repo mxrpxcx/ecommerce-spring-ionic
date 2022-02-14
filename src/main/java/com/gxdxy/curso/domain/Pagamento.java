aa1ce5edd037ecb9c8a2e9e20bb40a7b6a9e3c84
Godoy <renanfgfg@gmail.com>
2022-02-14 09:46:17 -0300

    mapeamento 1 para 1

--- src/main/java/com/gxdxy/curso/domain/Pagamento.java
+++ src/main/java/com/gxdxy/curso/domain/Pagamento.java
@@ -2,13 +2,25 @@ package com.gxdxy.curso.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
 import com.gxdxy.curso.domain.enums.EstadoPagamento;
 
+@Entity
 public class Pagamento implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
 	private Integer id;
 	private EstadoPagamento estado;
+	
+	@OneToOne
+	@JoinColumn(name="pedido_id")
+	@MapsId
 	private Pedido pedido;
 	
 	public Pagamento() {
--- src/main/java/com/gxdxy/curso/domain/Pedido.java
+++ src/main/java/com/gxdxy/curso/domain/Pedido.java
@@ -3,11 +3,23 @@ package com.gxdxy.curso.domain;
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Pedido implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private Date instante;
+	
+	@OneToOne(cascade=CascadeType.ALL, mappedBy="pedido")
 	private Pagamento pagamento;
 	private Cliente cliente;
 	private Endereco enderecoEntrega;
