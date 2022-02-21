dbadeaab4dab091fdacfacaf11405f5dd23d0531
Godoy <renanfgfg@gmail.com>
2022-02-21 16:17:33 -0300

    json ignore

--- src/main/java/com/gxdxy/curso/domain/Cliente.java
+++ src/main/java/com/gxdxy/curso/domain/Cliente.java
@@ -14,8 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.gxdxy.curso.domain.enums.TipoCliente;
 
 @Entity
@@ -30,11 +29,10 @@ public class Cliente implements Serializable {
 	private String documento;
 	private Integer tipo;
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	
