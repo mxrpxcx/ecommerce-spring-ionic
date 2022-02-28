c389d4449ee521ab01b2c9280da36f46062619fa
Godoy <renanfgfg@gmail.com>
2022-02-27 23:07:04 -0300

    post request adicionado para categorias

--- src/main/java/com/gxdxy/curso/resources/CategoriaResource.java
+++ src/main/java/com/gxdxy/curso/resources/CategoriaResource.java
@@ -1,11 +1,15 @@
 package com.gxdxy.curso.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gxdxy.curso.domain.Categoria;
 import com.gxdxy.curso.service.CategoriaService;
@@ -21,6 +25,15 @@ public class CategoriaResource {
 	public ResponseEntity<?> listar(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> inserir(@RequestBody Categoria obj){
+		obj = service.inserir(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+				buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 	
 }
--- src/main/java/com/gxdxy/curso/service/CategoriaService.java
+++ src/main/java/com/gxdxy/curso/service/CategoriaService.java
@@ -23,4 +23,9 @@ public class CategoriaService {
 		  	
 		  }
 	
+	public Categoria inserir(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
 }
