aa812c82c5382111bf56a3d5113e81c6c25800ab
Godoy <renanfgfg@gmail.com>
2022-02-28 00:28:10 -0300

    get mapping com dto para listar todas categorias

--- src/main/java/com/gxdxy/curso/resources/CategoriaResource.java
+++ src/main/java/com/gxdxy/curso/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.gxdxy.curso.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,44 +14,49 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gxdxy.curso.domain.Categoria;
+import com.gxdxy.curso.dto.CategoriaDTO;
 import com.gxdxy.curso.service.CategoriaService;
 
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
+
 	@Autowired
 	private CategoriaService service;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Categoria> listar(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> inserir(@RequestBody Categoria obj){
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> inserir(@RequestBody Categoria obj) {
 		obj = service.inserir(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
-				buildAndExpand(obj.getId()).toUri();
-		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
 		return ResponseEntity.created(uri).build();
 	}
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.PUT)
-	public ResponseEntity<Void> atualizar(@PathVariable Integer id, 
-			@RequestBody Categoria obj){
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> atualizar(@PathVariable Integer id, @RequestBody Categoria obj) {
 		obj.setId(id);
 		obj = service.atualizar(obj);
 		return ResponseEntity.noContent().build();
 	}
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> deletar(@PathVariable Integer id) {
 		service.deletar(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> listarTudo() {
+		List<Categoria> lista = service.buscarTudo();
+		List<CategoriaDTO> listDTO = lista.stream()
+				.map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+
 }
-
-
--- src/main/java/com/gxdxy/curso/service/CategoriaService.java
+++ src/main/java/com/gxdxy/curso/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.gxdxy.curso.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,6 +44,10 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
 		}
+	}
+	
+	public List<Categoria> buscarTudo() {
+		return repo.findAll();
 	}
 
 }
