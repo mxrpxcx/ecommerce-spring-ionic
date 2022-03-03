6d7f7904ed4ddacd7bc3bf3d94efcd0f2e78c99e
Godoy <renanfgfg@gmail.com>
2022-03-03 01:06:05 -0300

    delete em cascata para clientes sem pedidos

--- src/main/java/com/gxdxy/curso/domain/Cliente.java
+++ src/main/java/com/gxdxy/curso/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -33,7 +34,7 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
-	@OneToMany(mappedBy="cliente")
+	@OneToMany(mappedBy="cliente", cascade=CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 	
 	@ElementCollection
