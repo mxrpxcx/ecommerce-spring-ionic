7c657f615a3b121cd9b34daf879e30b338f31791
Godoy <renanfgfg@gmail.com>
2022-03-04 22:30:25 -0300

    criando profile de testes

--- src/main/java/com/gxdxy/curso/CursoJavaECommerceApplication.java
+++ src/main/java/com/gxdxy/curso/CursoJavaECommerceApplication.java
@@ -1,65 +1,13 @@
 package com.gxdxy.curso;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.gxdxy.curso.domain.Categoria;
-import com.gxdxy.curso.domain.Cidade;
-import com.gxdxy.curso.domain.Cliente;
-import com.gxdxy.curso.domain.Endereco;
-import com.gxdxy.curso.domain.Estado;
-import com.gxdxy.curso.domain.ItemPedido;
-import com.gxdxy.curso.domain.Pagamento;
-import com.gxdxy.curso.domain.PagamentoComBoleto;
-import com.gxdxy.curso.domain.PagamentoComCartao;
-import com.gxdxy.curso.domain.Pedido;
-import com.gxdxy.curso.domain.Produto;
-import com.gxdxy.curso.domain.enums.EstadoPagamento;
-import com.gxdxy.curso.domain.enums.TipoCliente;
-import com.gxdxy.curso.repository.CategoriaRepository;
-import com.gxdxy.curso.repository.CidadeRepository;
-import com.gxdxy.curso.repository.ClienteRepository;
-import com.gxdxy.curso.repository.EnderecoRepository;
-import com.gxdxy.curso.repository.EstadoRepository;
-import com.gxdxy.curso.repository.ItemPedidoRepository;
-import com.gxdxy.curso.repository.PagamentoRepository;
-import com.gxdxy.curso.repository.PedidoRepository;
-import com.gxdxy.curso.repository.ProdutoRepository;
-
 @SpringBootApplication
 public class CursoJavaECommerceApplication implements CommandLineRunner {
 
-	@Autowired
-	private CategoriaRepository categoriaRepo;
-	
-	@Autowired
-	private ProdutoRepository produtoRepo;
-	
-	@Autowired
-	private EstadoRepository estadoRepo;
-	
-	@Autowired
-	private CidadeRepository cidadeRepo;
-	
-	@Autowired
-	private ClienteRepository clienteRepo;
-	
-	@Autowired
-	private EnderecoRepository enderecoRepo;
-	
-	@Autowired
-	private PedidoRepository pedidoRepo;
-	
-	@Autowired
-	private PagamentoRepository pagamentoRepo;
-	
-	@Autowired
-	private ItemPedidoRepository itemPedidoRepo;
+
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoJavaECommerceApplication.class, args);
@@ -67,86 +15,7 @@ public class CursoJavaECommerceApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Categoria c1 = new Categoria(null, "Samsung");
-		Categoria c2 = new Categoria(null, "Apple");
-		Categoria c3 = new Categoria(null, "Xiaomi");
-		Categoria c4 = new Categoria(null, "Motorola");
-		Categoria c5 = new Categoria(null, "LG");
 		
-		Produto p1 = new Produto(null, "S20 FE", 1900.00); 
-		Produto p2 = new Produto(null, "iPhone SE", 2500.00);
-		Produto p3 = new Produto(null, "Mi 11", 3500.00);
-		
-		Produto p4 = new Produto(null, "Edge 30 PRO", 5500.00);
-		Produto p5 = new Produto(null, "S22 ULTRA", 15000.00);
-		Produto p6 = new Produto(null, "Velvet", 15000.00);
-		
-		c1.getProdutos().addAll(Arrays.asList(p1,p5));
-		c2.getProdutos().add(p2);
-		c3.getProdutos().add(p3);
-		c4.getProdutos().add(p4);
-		c5.getProdutos().add(p6);
-		
-		p1.getCategorias().add(c1);
-		p2.getCategorias().add(c2);
-		p3.getCategorias().add(c3);
-		p4.getCategorias().add(c4);
-		p5.getCategorias().add(c1);
-		p6.getCategorias().add(c5);
-		
-		categoriaRepo.saveAll(Arrays.asList(c1,c2, c3, c4, c5));
-		produtoRepo.saveAll(Arrays.asList(p1,p2,p3,p4,p5,p6));
-		
-		Estado e1 = new Estado(null, "Minas Gerais");
-		Estado e2 = new Estado(null, "São Paulo");
-		
-		Cidade cid1 = new Cidade(null, "Uberlandia", e1);
-		Cidade cid2 = new Cidade(null, "São Paulo", e2);
-		Cidade cid3 = new Cidade(null, "Campinas", e2);
-		
-		e1.getCidades().add(cid1);
-		e2.getCidades().addAll(Arrays.asList(cid2,cid3));
-		
-		estadoRepo.saveAll(Arrays.asList(e1,e2));
-		cidadeRepo.saveAll(Arrays.asList(cid1,cid2,cid3));
-		
-		Cliente cli1 = new Cliente(null,"Maria Silva","M@M.COM","119", TipoCliente.PESSOAFISICA);
-		cli1.getTelefones().addAll(Arrays.asList("190","192"));
-		Endereco end1 = new Endereco(null,"Rua Flores","300","Apto 303","Jardim","3030",cli1,cid1);
-		Endereco end2 = new Endereco(null,"Avenida Matos","105","Sala 800","Centro","3031",cli1,cid2);
-
-		cli1.getEnderecos().addAll(Arrays.asList(end1,end2));
-		
-		clienteRepo.saveAll(Arrays.asList(cli1));
-		enderecoRepo.saveAll(Arrays.asList(end1,end2));
-		
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm");
-		Pedido ped1 = new Pedido(null,sdf.parse("01/01/2022 10:30"),cli1,end1);
-		Pedido ped2 = new Pedido(null,sdf.parse("05/01/2022 21:20"),cli1,end2);
-		
-		Pagamento pag1 = new PagamentoComCartao(null,EstadoPagamento.QUITADO,ped1,6);
-		ped1.setPagamento(pag1);
-		
-		Pagamento pag2 = new PagamentoComBoleto(null,EstadoPagamento.PENDENTE,ped2,sdf.parse("05/05/2022 00:00"),null);
-		ped2.setPagamento(pag2);
-		
-		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
-		
-		pedidoRepo.saveAll(Arrays.asList(ped1,ped2));
-		pagamentoRepo.saveAll(Arrays.asList(pag1,pag2));
-		
-		ItemPedido ip1 = new ItemPedido(ped1,p1,0.00,1,p1.getPreco());
-		ItemPedido ip2 = new ItemPedido(ped1,p3,0.00,1,p3.getPreco());
-		ItemPedido ip3 = new ItemPedido(ped2,p2,100.00,1,p2.getPreco());
-		
-		ped1.getItens().addAll(Arrays.asList(ip1,ip2));
-		ped2.getItens().add(ip3);
-		
-		p1.getItens().add(ip1);
-		p2.getItens().add(ip3);
-		p3.getItens().add(ip2);
-		
-		itemPedidoRepo.saveAll(Arrays.asList(ip1,ip2,ip3));
 		
 	}
 
---
+++ src/main/java/com/gxdxy/curso/config/TestConfig.java
@@ -0,0 +1,25 @@
+package com.gxdxy.curso.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.gxdxy.curso.service.DbService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DbService dbService;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+}
